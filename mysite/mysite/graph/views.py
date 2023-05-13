9f3bbad5c7d930d7b330894490214cd60cf8c5cf
Yann <yvillellas@gmail.com>
2023-05-13 19:10:56 +0200

    debut

--- mysite/mysite/graph/views.py
+++ mysite/mysite/graph/views.py
@@ -1,7 +1,72 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
+import pandas as pd
+import plotly.express as px
+import requests
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the graph index.")
+    # On importe le fichier csv
+    dvf2022 = pd.read_csv("valeursfoncieres-2022.txt", sep="|", low_memory=False)
+
+    #Suppression des colonnes inutiles pour les demandes des valeurs foncières de 2022
+    dvf2022 = dvf2022.drop(['Identifiant de document', 'Reference document', '1 Articles CGI', '2 Articles CGI', '3 Articles CGI', '4 Articles CGI','5 Articles CGI', 'No disposition', 'No voie'], axis=1)
+    dvf2022 = dvf2022.drop(['B/T/Q', 'Type de voie', 'Code voie', 'Code commune', 'Prefixe de section', 'Section', 'No plan', 'No Volume'], axis=1)
+    dvf2022 = dvf2022.drop(['Voie','Nature culture speciale', 'Identifiant local', 'Nombre de lots', '3eme lot', 'Surface Carrez du 3eme lot', '5eme lot', 'Surface Carrez du 5eme lot', '4eme lot', 'Surface Carrez du 4eme lot'], axis=1)
+
+    #ValueError: could not convert string to float: '55000,00'
+    dvf2022['Valeur fonciere'] = dvf2022['Valeur fonciere'].str.replace(',', '.').astype(float)
+
+    #On avait une erreur vu qu'on pouvait pas calculer sur des strings
+    dvf2022['Surface reelle bati'] = dvf2022['Surface reelle bati'].astype(float)
+
+    #On créé une nouvelle colonne qui contient le prix au mètre carré qui est la sormme des Surface Carrez
+    dvf2022['Surface Carrez du 1er lot'] = dvf2022['Surface Carrez du 1er lot'].str.replace(',', '.').astype(float)
+    dvf2022['Surface Carrez du 2eme lot'] = dvf2022['Surface Carrez du 2eme lot'].str.replace(',', '.').astype(float)
+
+    #On remplace toutes les valeurs nulles par 0
+    dvf2022 = dvf2022.fillna(0)
+
+    #On créé une nouvelle colonne qui contient le prix au mètre carré qui est la sormme des Surface Carrez
+    dvf2022['Metre carre'] = dvf2022['Surface Carrez du 1er lot'].astype(float) + dvf2022['Surface Carrez du 2eme lot'].astype(float)
+
+    #Enleve les valeurs en double
+    dvf2022 = dvf2022.drop_duplicates(subset=['Date mutation', 'Valeur fonciere', 'Surface reelle bati', 'Metre carre'], keep='first')
+
+    # On fait un nouveau tableau contenant que des "Metre carre" non nuls
+    dvf2022_metre_carre = dvf2022[dvf2022['Metre carre'] >= 1]
+
+    #On remplace le nom des communes avec un - par un espace
+    dvf2022_metre_carre['Commune'] = dvf2022_metre_carre['Commune'].str.replace('-', ' ')
+
+    nombre_de_vente_par_departement = dvf2022.groupby('Code departement')['Valeur fonciere'].count()
+
+    #On renomme les colonnes
+    nombre_de_vente_par_departement = nombre_de_vente_par_departement.reset_index()
+    nombre_de_vente_par_departement = nombre_de_vente_par_departement.rename(columns={'Code departement': 'Département', 'Valeur fonciere': 'Nombre de ventes'})
+
+    #On fait un geojson avec les départements
+    departement_geojson_url = "https://france-geojson.gregoiredavid.fr/repo/departements.geojson"
+    departement_geojson = requests.get(departement_geojson_url).json()
+
+
+    fig = px.choropleth(nombre_de_vente_par_departement, 
+                        geojson=departement_geojson, 
+                        locations='Département', 
+                        color='Nombre de ventes',
+                        color_continuous_scale='pinkyl',
+                        featureidkey='properties.code',
+                        projection="mercator",
+                        title='Nombre de ventes par département en France')
+    fig.update_geos(fitbounds="locations", visible=False)
+    fig.update_layout(height=600, width=800)
+    fig.show()
+
+    template = loader.get_template("template0.html")
+    context = {
+        "nombre_de_vente_par_departement": nombre_de_vente_par_departement.to_html(),
+    }
+    return HttpResponse(template.render(context, request))
