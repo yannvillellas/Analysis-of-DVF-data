d7dcb8c6b52cb637367a6e7a734c36b1ffe8f628
Yann <yvillellas@gmail.com>
2023-05-14 18:37:45 +0200

    homepage with form

--- mysite/mysite/graph/urls.py
+++ mysite/mysite/graph/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.homepage, name="homepage"),
+    path("prixMoyen/", views.prixMoyen, name="prixMoyen"),
+    path("nombreVente/", views.nombreVente, name="nombreVente"),
 ]
