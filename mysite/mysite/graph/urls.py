903a16ce608bcacd461880c33949dc08f6b2907b
Yann <yvillellas@gmail.com>
2023-05-15 21:26:49 +0200

    modified urls

--- mysite/mysite/graph/urls.py
+++ mysite/mysite/graph/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.homepage, name="homepage"),
+    path("graph/", views.homepage, name="homepage"),
     path("prixMoyen/", views.prixMoyen, name="prixMoyen"),
     path("nombreVente/", views.nombreVente, name="nombreVente"),
 ]
